62bb097800e2e9dcf2b208375788c55d590d54f2
Sogand <sogandmohamadipour7@gmail.com>
2026-06-17 12:20:30 +0330

    base structure

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useMemo, useState } from "react";
+import debounce from "./utils/debounce";
+import throttle from "./utils/Throttle";
+
+export default function App() {
+  const [valueD, setValueD] = useState("");
+  const [valueth, setValueth] = useState("");
+
+  // throttle ------------
+  const throttledLog = useMemo(
+    () =>
+      throttle((value) => {
+        console.log("Searching:", value);
+      }, 2000),
+    [],
+  );
+
+  function handleChangeth(e) {
+    setValueth(e.target.value);
+    throttledLog(e.target.value);
+  }
+
+
+  //dbounce ----------------
+  const debouncedLog = debounce((inputValue) => {
+    console.log("value:", inputValue);
+  }, 2000);
+
+  function handleChange(e) {
+    setValueD(e.target.value);
+    debouncedLog(e.target.value);
+  }
+  // ------------
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={valueD}
+        onChange={handleChange}
+        placeholder="debounce..."
+      />
+      <input
+        type="text"
+        value={valueth}
+        onChange={handleChangeth}
+        placeholder="throttle..."
+      />
+    </div>
+  );
+}
---
+++ src/utils/debounce.js
@@ -0,0 +1,11 @@
+export default function debounce(func, delay) {
+    let timer;
+
+    return function (...args) {
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
+}
